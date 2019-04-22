aa9f6857cb740c32d6ca9669aebd63509242d93c
Lester Dowling <Lester.Dowling@gmail.com>
2019-04-23 09:57:12 +1000

    Regularize and clean-up grammar definitions.

--- Pseudo-XPath/public/Pseudo-XPath/mini-grammar.hpp
+++ Pseudo-XPath/public/Pseudo-XPath/mini-grammar.hpp
@@ -33,63 +33,58 @@ namespace pseudo_xpath {
 	constexpr char kSQ = '\'';
 
 	template<typename Iterator>
-	class mini_grammar : public qi::grammar<Iterator, void(), ascii::space_type> //
+	class mini_grammar
+	  : public qi::grammar<Iterator, void(), ascii::space_type> //
 	{
-		using base_type = typename mini_grammar::base_type;
-		using start_type = typename base_type::start_type;
-		using attr_type = typename start_type::attr_type;
+		using Base_Type = typename mini_grammar::base_type;
+		using Start_Type = typename Base_Type::start_type;
+		using Attr_Type = typename Start_Type::attr_type;
 		using This = typename mini_grammar<Iterator>;
 
-		using char_seq_t = std::vector<char>;
-		using char_seq_rule_t = qi::rule<Iterator, char_seq_t(), ascii::space_type>;
-		using quoted_attr_t = fusion::vector<char_seq_t, int>;
-		using quoted_seq_rule_t = qi::rule<Iterator, quoted_attr_t(), ascii::space_type>;
-		using filter_attr_t = fusion::vector<char_seq_t, char, quoted_attr_t>;
+		using Char_Seq = std::vector<char>;
+		using Char_Seq_Rule = qi::rule<Iterator, Char_Seq(), ascii::space_type>;
+		using Quoted_Tuple = fusion::vector<Char_Seq, int>;
+		using Quoted_Seq_Rule = qi::rule<Iterator, Quoted_Tuple(), ascii::space_type>;
+		using Filter_Attr = fusion::vector<Char_Seq, char, Quoted_Tuple>;
 
-	  public:
+	public:
 		/**
 		 * The parsed result of this grammar.
 		 */
 		Grade::SP result{ new Grade };
 
-	  private:
+	private:
 		Grade_Creator::SP creator{ new Grade_Creator{ result } };
-		char_seq_rule_t xml_identifier;
-		char_seq_rule_t attribute_name;
-		quoted_seq_rule_t attribute_value_dq; // A double-quoted attribute value.
-		quoted_seq_rule_t attribute_value_sq; // A single-quoted attribute value.
-		quoted_seq_rule_t attribute_value_nq; // An unquoted attribute value.
-		quoted_seq_rule_t attribute_value_dg; // An attribute value with only digits.
-		quoted_seq_rule_t implicit_value;
-		quoted_seq_rule_t attribute_value;
+		Char_Seq_Rule xml_identifier;
+		Char_Seq_Rule attribute_name;
+		Quoted_Seq_Rule attribute_value_dq; // A double-quoted attribute value.
+		Quoted_Seq_Rule attribute_value_sq; // A single-quoted attribute value.
+		Quoted_Seq_Rule attribute_value_nq; // An unquoted attribute value.
+		Quoted_Seq_Rule attribute_value_dg; // An attribute value with only digits.
+		Quoted_Seq_Rule implicit_value;		// Implicit attribute value for name or idx.
+		Quoted_Seq_Rule attribute_value;
 		const int dq_flag{ 1 }; // Flag for double quoted value.
 		const int sq_flag{ 2 }; // Flag for single quoted value.
 		const int nq_flag{ 3 }; // Flag for unquoted value (no quote).
 		const int dg_flag{ 4 }; // Flag for value with digits only.
-		enum Quote_Type : int
-		{
-			kDoubleQuoted = 1,
-			kSingleQuoted,
-			kNotQuoted,
-			kDigitsOnly
-		};
-		qi::rule<Iterator, filter_attr_t(), ascii::space_type> binary_filter;
+		enum Quote_Type : int { kDoubleQuoted = 1, kSingleQuoted, kNotQuoted, kDigitsOnly };
+		qi::rule<Iterator, Filter_Attr(), ascii::space_type> binary_filter;
 		qi::rule<Iterator, void(), ascii::space_type> attributes;
-		start_type start;
+		Start_Type start;
 
-		char_seq_t remainder;
-		void set_remainder(char_seq_t const& r) { remainder = r; }
+		// Char_Seq remainder;
+		// void set_remainder(Char_Seq const& r) { remainder = r; }
 
 		/**
 		 * A component part of a pseudo XPath was successfully parsed, so
 		 * add it to the list of @c Grades.
 		 */
-		void add_filter(filter_attr_t const& v)
+		void add_filter(Filter_Attr const& v)
 		{
 			const std::string attribute_name{ fusion::at_c<0>(v).begin(),
 											  fusion::at_c<0>(v).end() };
 			const char filter_operator{ fusion::at_c<1>(v) };
-			const quoted_attr_t& quoted_attr = fusion::at_c<2>(v);
+			const Quoted_Tuple& quoted_attr = fusion::at_c<2>(v);
 			const std::string filter_value{ fusion::at_c<0>(quoted_attr).begin(),
 											fusion::at_c<0>(quoted_attr).end() };
 			const int quote_type = fusion::at_c<1>(quoted_attr);
@@ -103,7 +98,7 @@ namespace pseudo_xpath {
 		 * An implicitly given filter within a pseudo XPath was successfully parsed,
 		 * so add it to the list of @c Grades.
 		 */
-		void add_implicit_filter(quoted_attr_t const& v)
+		void add_implicit_filter(Quoted_Tuple const& v)
 		{
 			const std::string attribute_name{};
 			const char filter_operator{ '=' };
@@ -116,9 +111,9 @@ namespace pseudo_xpath {
 			  attribute_name, filter_operator, filter_value, quoted, digits_only);
 		}
 
-	  public:
+	public:
 		mini_grammar()
-		  : base_type{ start }
+		  : Base_Type{ start }
 		  , xml_identifier{ qi::lexeme[+qi::char_("a-zA-Z0-9:_-")] }
 		  , attribute_name{ qi::lexeme[+qi::char_("a-zA-Z0-9:_-")] }
 		  , attribute_value_dq{ qi::lit(kDQ) >> qi::no_skip[*(qi::char_ - kDQ)] >
--- Simple-XML/public/Simple-XML/mini-grammar.hpp
+++ Simple-XML/public/Simple-XML/mini-grammar.hpp
@@ -31,37 +31,42 @@ namespace simple_xml {
 	namespace phoenix = boost::phoenix;
 	namespace qi = boost::spirit::qi;
 	namespace ascii = boost::spirit::ascii;
+	constexpr char kDQ = '"';
+	constexpr char kSQ = '\'';
 
 	template<typename Iterator>
-	struct mini_grammar
-	  : qi::grammar<Iterator, void(), ascii::space_type> //
+	class mini_grammar
+	  : public qi::grammar<Iterator, void(), ascii::space_type> //
 	{
-		using base_type = typename mini_grammar::base_type;
-		using start_type = typename base_type::start_type;
-		using attr_type = typename start_type::attr_type;
+		using Base_Type = typename mini_grammar::base_type;
+		using Start_Type = typename Base_Type::start_type;
+		using Attr_Type = typename Start_Type::attr_type;
+		using This = mini_grammar<Iterator>;
+		using Char_Seq = std::vector<char>;
+		using Char_Seq_Rule = qi::rule<Iterator, Char_Seq(), ascii::space_type>;
+		using Assignment_Rule =
+		  qi::rule<Iterator, fusion::vector<Char_Seq, Char_Seq>(), ascii::space_type>;
 
 		Element_Creator *creator;
-		start_type start;
-		start_type tail;
-		start_type element;
-		start_type children;
-		start_type sibling;
-		start_type closing_tag;
-		start_type header;
-
-		typedef std::vector<char> CharSeq;
-		qi::rule<Iterator, CharSeq(), ascii::space_type> xml_identifier;
-		qi::rule<Iterator, CharSeq(), ascii::space_type> text_node;
-		qi::rule<Iterator, CharSeq(), ascii::space_type> attribute_name;
-		qi::rule<Iterator, CharSeq(), ascii::space_type> attribute_value;
-		qi::rule<Iterator, fusion::vector<CharSeq, CharSeq>(), ascii::space_type>
-		  assignment;
-		start_type attributes;
+		Char_Seq_Rule xml_identifier;
+		Char_Seq_Rule attribute_name;
+		Char_Seq_Rule attribute_value;
+		Assignment_Rule assignment;
+		Start_Type attributes;
+		Char_Seq_Rule text_node;
+		Start_Type closing_tag;
+		Start_Type xml_header;
+		Start_Type tail;
+		// The following rules are recursive:
+		Start_Type sibling;
+		Start_Type children;
+		Start_Type element;
+		Start_Type start;
 
 		std::vector<char> remainder;
 		void set_remainder(std::vector<char> y) { remainder = y; }
 
-		void set_attribute(fusion::vector<CharSeq, CharSeq> const &v)
+		void set_attribute(fusion::vector<Char_Seq, Char_Seq> const &v)
 		{
 			const std::string attribute_name{ fusion::at_c<0>(v).begin(),
 											  fusion::at_c<0>(v).end() };
@@ -70,51 +75,45 @@ namespace simple_xml {
 			creator->set_attribute(attribute_name, attribute_value);
 		}
 
+	public:
 		mini_grammar(Element_Creator *element_creator)
-		  : base_type(start, "start")
+		  : Base_Type(start, "start")
 		  , creator{ element_creator }
+		  , xml_identifier{ qi::lexeme[+qi::char_("a-zA-Z0-9:_-")] }
+		  , attribute_name{ qi::lexeme[+qi::char_("a-zA-Z0-9:_-")] }
+		  , attribute_value{ qi::lit(kDQ) >> qi::no_skip[*(qi::char_ - kDQ)] >
+							 qi::lit(kDQ) }
+		  , assignment{ attribute_name > qi::lit('=') > attribute_value, "assignment" }
+		  , attributes{ *(assignment[boost::bind(&This::set_attribute, this, _1)]) }
+		  , text_node{ qi::lexeme[+(qi::char_ - '<' - '>')]
+								 [boost::bind(&Element_Creator::set_text, creator, _1)] }
+		  , closing_tag{ qi::lit("</") >> //
+						   xml_identifier[boost::bind(
+							 &Element_Creator::verify_closing_tag,
+							 creator,
+							 _1)] > qi::lit('>'),
+						 "closing_tag" }
+		  , xml_header{ qi::lit("<?") > qi::omit[+(qi::char_ - '?' - '<' - '>')] >
+						  qi::lit("?>"),
+						"xml-header" }
+		  , tail{ qi::lexeme[*ascii::print][boost::bind(&This::set_remainder, this, _1)] }
 		{
-			typedef mini_grammar<Iterator> This;
-
-			xml_identifier %= qi::lexeme[+qi::char_("a-zA-Z0-9:_-")];
-			attribute_name %= qi::lexeme[+qi::char_("a-zA-Z0-9:_-")];
-			attribute_value %=
-			  qi::lit('"') >> qi::no_skip[*(qi::char_ - '"')] > qi::lit('"');
-			assignment %= attribute_name > qi::lit('=') > attribute_value;
-			attributes = *(assignment[boost::bind(&This::set_attribute, this, _1)]);
-
-			element =
+			element %=
 			  qi::lit('<') >>
 			  xml_identifier[boost::bind(&Element_Creator::new_element, creator, _1)] >>
 			  attributes >>
 			  (qi::lit("/>")[boost::bind(&Element_Creator::close_singleton, creator)] |
 			   (qi::lit('>') >> children >> closing_tag));
 
-			closing_tag = qi::lit("</") >>
-						  xml_identifier[boost::bind(
-							&Element_Creator::verify_closing_tag, creator, _1)] >
-						  qi::lit('>');
-
-			text_node = qi::lexeme[+(qi::char_ - '<' - '>')]
-								  [boost::bind(&Element_Creator::set_text, creator, _1)];
-
 			sibling = element | text_node;
 
-			children = *(sibling);
+			children = *sibling;
 
-			header =
-			  qi::lit("<?") > qi::omit[+(qi::char_ - '?' - '<' - '>')] > qi::lit("?>");
+			start = *xml_header > element >> tail;
 
-			start = *header > element >> tail;
-
-			tail = qi::lexeme[*ascii::print][boost::bind(&This::set_remainder, this, _1)];
-			start.name("start");
 			element.name("element");
 			children.name("children");
 			sibling.name("sibling");
-			closing_tag.name("closing_tag");
-			text_node.name("text_node");
-			xml_identifier.name("xml_identifier");
 
 			qi::on_error<qi::fail>(
 			  start,
